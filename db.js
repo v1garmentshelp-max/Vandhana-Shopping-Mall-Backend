7606b3dc89350f630ea5cdcf3fed432e265303e7
Puli Venkata Ganesh <ganeshyadav.puli9010@gmail.com>
2026-05-03 13:23:48 +0530

    Removed hardcoded DB credentials

--- db.js
+++ db.js
@@ -1,12 +1,8 @@
 require('dotenv').config()
 const { Pool } = require('pg')
 
-const connectionString =
-  process.env.DATABASE_URL ||
-  'postgresql://neondb_owner:npg_L4WCoHgsFbt1@ep-lingering-lab-a1plroab-pooler.ap-southeast-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require'
-
 const pool = new Pool({
-  connectionString,
+  connectionString: process.env.DATABASE_URL,
   ssl: { rejectUnauthorized: false },
   max: 5,
   idleTimeoutMillis: 30000,
