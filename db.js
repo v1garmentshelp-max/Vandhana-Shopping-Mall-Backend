9f951a994b9ab3461f8137be5a3846275a20a38b
Puli Venkata Ganesh <ganeshyadav.puli9010@gmail.com>
2026-05-03 13:14:09 +0530

    using the db url directly in the code

--- db.js
+++ db.js
@@ -1,8 +1,12 @@
 require('dotenv').config()
 const { Pool } = require('pg')
 
+const connectionString =
+  process.env.DATABASE_URL ||
+  'postgresql://neondb_owner:npg_L4WCoHgsFbt1@ep-lingering-lab-a1plroab-pooler.ap-southeast-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require'
+
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
+  connectionString,
   ssl: { rejectUnauthorized: false },
   max: 5,
   idleTimeoutMillis: 30000,
