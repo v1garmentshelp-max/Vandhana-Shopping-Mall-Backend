5ee300e025004df6d28c5c57b875cec32cd4aa28
Puli Venkata Ganesh <ganeshyadav.puli9010@gmail.com>
2026-06-28 00:58:24 +0530

    db issue fixed

--- db.js
+++ db.js
@@ -1,13 +1,27 @@
 const { Pool } = require('pg')
 
+const rawConnectionString = process.env.DATABASE_URL
+
+if (!rawConnectionString) {
+  throw new Error('DATABASE_URL is missing')
+}
+
+if (!rawConnectionString.startsWith('postgres://') && !rawConnectionString.startsWith('postgresql://')) {
+  throw new Error('DATABASE_URL must start with postgres:// or postgresql://')
+}
+
+const connectionString = rawConnectionString.replace(/\?sslmode=require$/, '')
+
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
+  connectionString,
   ssl: {
     rejectUnauthorized: false
   },
   max: 5,
   idleTimeoutMillis: 30000,
-  connectionTimeoutMillis: 10000
+  connectionTimeoutMillis: 5000,
+  query_timeout: 30000,
+  statement_timeout: 30000
 })
 
 module.exports = pool
